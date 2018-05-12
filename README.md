8fd47254efc3143c72978657a205b9938a93e57f
OriginCode <origincoder@yahoo.com>
2018-05-12 15:31:29 +0800

    1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sync your shadowsocks-libev profile to your phone through a simple QR code!
+
+## Requirements
+Python 3, qrcode
+
+## Design from @KenOokamiHoro
---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import json
+import base64
+import qrcode
+import os
+import re # 來湊熱鬧的
+
+try:
+    f = open(sys.argv[1])
+except:
+    f = open("config.json")
+    print('No specific config file. Using default (./config.json).')
+
+config = json.load(f)
+encodestr = config['method'] + ':' + config['password']
+srcstr = "ss://%s@%s:%s#%s" % (re.match(r"b'(.*)'", str(base64.b64encode(encodestr.encode()))).group(1), config['server'], config['server_port'], 'default')
+qrcode.make(srcstr).save('image.png')
+
+print('Image has been saved as ./image.png !')
+answer = input('Open the QR Code? [Y/n] ')
+
+if answer == "n" or answer == "N":
+    exit(0)
+elif answer == "y" or answer == "Y":
+    os.system('xdg-open image.png')
+elif answer == "":
+    os.system('xdg-open image.png')
+else:
+    print('Invalid option: %s' % answer)
